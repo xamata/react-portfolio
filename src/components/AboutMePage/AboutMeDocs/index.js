4a10a278a4ed930d93c2776a467652eabed16879
xamata <61128570+xamata@users.noreply.github.com>
2022-02-11 11:14:38 -0800

    Updated the link to my resume

--- src/components/AboutMePage/AboutMeDocs/index.js
+++ src/components/AboutMePage/AboutMeDocs/index.js
@@ -20,7 +20,7 @@ const AboutMeDocuments = ({ lightBg, id, img, alt, imgStart, topline, lightText,
                                 <Heading lightText={lightText}>{headline}</Heading>
                                 <Subtitle darkText={darkText}>{description}</Subtitle>
                                 <BtnWrap>
-                                    <ResumeLink href="//drive.google.com/drive/u/2/folders/1PZoHb7fkb5tbn_OFpVOONDNtzdgNexhm" target='_blank' aria-label='Resume'>
+                                    <ResumeLink href="//drive.google.com/file/d/168wyviWx9xQ1vOYUupXlwU7_BzKkp-Bq/view?usp=sharing" target='_blank' aria-label='Resume'>
                                         <ButtonDiv
                                             spy={true}
                                             primary={primary ? 1 : 0}
